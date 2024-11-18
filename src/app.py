b6e0312a5badfa34e7a5a7420e67bce1d1e6ea0b
Timo Mäkelä <timo.v.makela@helsinki.fi>
2024-11-18 21:35:07 +0200

    Added custom value input for counter

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request
 from counter import Counter
 
 app = Flask(__name__)
@@ -16,4 +16,9 @@ def reset():
 @app.route("/increment", methods=["POST"])
 def increment():
     cnt.increase()
+    return redirect("/")
+
+@app.route("/set_value", methods=["POST"])
+def set_value():
+    cnt.value = int(request.form["new_value"])
     return redirect("/")
